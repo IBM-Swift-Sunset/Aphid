46f9ca3569e68c358d7e5dfc3a36166bb807b675
Aaron <aaron.liberatore@gmail.com>
2016-07-27 13:21:35 -0500

    Switches to 0.7

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Aphid",
     dependencies: [
-        .Package(url: "https://github.com/sandmman/BlueSSLService.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 7),
     ],
     exclude: ["Aphid.xcodeproj", "README.md", "Sources/Info.plist"]
 )
