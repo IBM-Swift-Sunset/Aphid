6ede8e859bee8e0ca7488567ea38f9610fbd2086
Robert F. Dickerson <rfdickerson@gmail.com>
2016-09-26 15:23:31 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Aphid",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 11),
     ],
     exclude: ["Aphid.xcodeproj", "README.md", "Sources/Info.plist"]
 )
