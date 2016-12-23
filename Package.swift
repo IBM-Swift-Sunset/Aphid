2780ba6a4dd4763624c50e6d2969f48ccab24104
Robert F. Dickerson <rfdickerson@gmail.com>
2016-12-23 10:40:48 -0500

    Update to BlueSSL 0.12 (#19)
    * Update to BlueSSL 0.12
    
    * Update TravisCI
    
    * Change to XCode8

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Aphid",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 11),
+        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 12),
     ],
     exclude: ["Aphid.xcodeproj", "README.md", "Sources/Info.plist"]
 )
